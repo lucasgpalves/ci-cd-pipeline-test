11fd654724854944873fa6139420c6016a33aacf
Lucas Alves <alvez.lucas2702@gmail.com>
2025-07-29 22:01:01 -0300

    Inserindo main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    print("Server is running on http://localhost:8000")
+    print("Access the API at the root endpoint '/'")
